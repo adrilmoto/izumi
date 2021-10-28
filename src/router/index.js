5161e27549268e2ace40386109fca0901f7b125d
ivanq3w <ivanq3w@gmail.com>
2021-10-28 23:51:41 +0500

    Fixed routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,8 +10,8 @@ const routes = [
     children: [
       { meta: { name: 'ИНТРО', color: '#6362A0' }, path: '/', name: 'ИНТРО', component: () => import(/* webpackChunkName: "about" */ '../pages/Gif.vue') },
       { meta: { name: 'О НАС', color: '#EABE44' }, path: '/about', name: 'О НАС', component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue') },
-      { meta: { name: 'УСЛУГИ', color: '#52427A' }, path: '/services', name: 'УСЛУГИ', component: () => import(/* webpackChunkName: "servises" */ '../pages/Services.vue') },
       { meta: { name: 'КЕЙСЫ', color: '#CE5236' }, path: '/cases', name: 'КЕЙСЫ', component: () => import(/* webpackChunkName: "cases" */ '../pages/Cases.vue') },
+      { meta: { name: 'УСЛУГИ', color: '#52427A' }, path: '/services', name: 'УСЛУГИ', component: () => import(/* webpackChunkName: "servises" */ '../pages/Services.vue') },
       { meta: null, path: '/cases/:id', name: 'Case', component: () => import('../pages/Case.vue') },
       // { path: '/community', name: 'Комьюнити', component: () => import(/* webpackChunkName: "faces" */ '../pages/Community.vue') },
     ]
